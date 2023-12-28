f839afe26caf7e883d325cd8378e393f57ab5a7c
YoshimasaAsada <yoshimasa111112@gmail.com>
2023-12-28 14:49:07 +0900

    prismaについてのコメント追加

--- api/bookers2-api/src/prisma/prisma.module.ts
+++ api/bookers2-api/src/prisma/prisma.module.ts
@@ -4,5 +4,6 @@ import { PrismaService } from './prisma.service';
 @Module({
   providers: [PrismaService],
   exports: [PrismaService],
+  // 他のサービスで使う時、moduleでインポートするだけで使えるようにする
 })
 export class PrismaModule {}
