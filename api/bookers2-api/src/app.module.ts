59fb6dd434daf4d05a733161ebe48dbea80e4566
YoshimasaAsada <yoshimasa111112@gmail.com>
2023-12-29 01:27:33 +0900

    userのサービス雛形追加

--- api/bookers2-api/src/app.module.ts
+++ api/bookers2-api/src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), AuthModule, PrismaModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), AuthModule, PrismaModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
