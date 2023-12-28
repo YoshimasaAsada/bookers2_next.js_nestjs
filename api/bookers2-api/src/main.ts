108a0d6da35b5c3c47b0c49d6f76e40eef38c6ea
YoshimasaAsada <yoshimasa111112@gmail.com>
2023-12-28 11:41:20 +0900

    prisma導入

--- api/bookers2-api/src/main.ts
+++ api/bookers2-api/src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  /* バリデーション使えるようにする */
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+
+  /* corsの設定 */
+  app.enableCors({
+    credentials: true,
+    // 認証情報（クッキーやHTTP認証など）を許可するためのオプション
+    origin: ['http://localhost:4000/'],
+  });
+  
   await app.listen(3000);
 }
 bootstrap();
--- api/bookers2-api/src/prisma/prisma.service.ts
+++ api/bookers2-api/src/prisma/prisma.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PrismaService {}
+export class PrismaService extends PrismaClient {
+  // PrismaClientをここのprismaserviceに継承させる
+  constructor(private readonly config: ConfigService) {
+    // DBとの接続確立。.envにあるやつを採用
+    super({
+      datasources: {
+        db: {
+          url: config.get('DATABASE_URL'),
+        },
+      },
+    });
+  }
+}
