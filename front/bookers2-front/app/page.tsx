dfc0fcd306a1e6ae9781ddb8beb9acda8cfc801c
YoshimasaAsada <yoshimasa111112@gmail.com>
2024-01-05 04:45:01 +0900

    レイアウトいい感じに修正

--- front/bookers2-front/app/page.tsx
+++ front/bookers2-front/app/page.tsx
@@ -5,7 +5,6 @@ export default function Home() {
   return (
     <>
       <div className="relative h-screen overflow-hidden">
-        <div className="absolute inset-0 bg-black opacity-25"></div>
         <div className="container relative z-10 flex items-center px-6 py-32 mx-auto md:px-12 xl:py-40">
           <div className="relative z-10 flex flex-col items-start lg:w-3/5 xl:w-2/5">
             <h1 className="mt-4 text-6xl font-bold leading-tight text-white sm:text-7xl">
@@ -15,9 +14,11 @@ export default function Home() {
             </h1>
             <Link
               href="log-in"
-              className="block px-4 py-3 mt-10 text-lg font-bold text-gray-800 uppercase bg-white rounded-lg hover:bg-gray-100"
+              className="relative mt-10 text-lg font-bold uppercase inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800"
             >
-              Lgoin or SignIn
+              <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+                Lgoin or SignIn
+              </span>
             </Link>
           </div>
         </div>
