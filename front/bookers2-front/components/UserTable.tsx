6ce5fbd0ff866097160ac3c0c2a7b152b15085dd
YoshimasaAsada <yoshimasa111112@gmail.com>
2024-01-05 04:49:56 +0900

    ユーザー一覧のテーブルコンポーネント作成

---
+++ front/bookers2-front/components/UserTable.tsx
@@ -0,0 +1,39 @@
+import { User } from "@prisma/client";
+import React from "react";
+
+type Props = {
+  allUsers: User[];
+};
+
+const UserTable = (props: Props) => {
+  return (
+    <>
+      <div className="relative overflow-x-auto">
+        <table className="w-full text-sm text-left rtl:text-right">
+          <thead className="text-xs text-gray-700 dark:text-gray-400 border-b">
+            <tr>
+              <th scope="col" className="px-6 py-3">
+                name
+              </th>
+              <th scope="col" className="px-6 py-3">
+                email
+              </th>
+            </tr>
+          </thead>
+          <tbody className="">
+            {props.allUsers.map((user: User, index: number) => {
+              return (
+                <tr className="border-b dark:border-gray-700" key={index}>
+                  <td className="px-6 py-4">{user.name}</td>
+                  <td className="px-6 py-4">{user.email}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+    </>
+  );
+};
+
+export default UserTable;
