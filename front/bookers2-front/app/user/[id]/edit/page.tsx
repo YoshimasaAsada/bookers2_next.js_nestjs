14c7e5bca67b5973493c6b4338c2b4e3e1738e84
YoshimasaAsada <yoshimasa111112@gmail.com>
2024-01-09 04:58:47 +0900

    userのedit画面作成

--- front/bookers2-front/app/user/[id]/edit/page.tsx
+++ front/bookers2-front/app/user/[id]/edit/page.tsx
@@ -1,7 +1,36 @@
-import React from "react";
+"use client";
 
+import { EditUserForm } from "@/components/EditUserForm";
+import { useQueryBook } from "@/hooks/useQueryBook";
+import { useQueryUser } from "@/hooks/useQueryUser";
+import { CircularProgress } from "@mui/material";
+import { useParams } from "next/navigation";
+import React, { useEffect, useState } from "react";
 const page = () => {
-  return <div>page</div>;
+  const params = useParams();
+  const { queryUserById } = useQueryUser();
+  const { data, status } = queryUserById(params.id);
+  const user = data ?? "";
+
+  if (status === "loading")
+  return (
+    <>
+      <div className="h-screen w-screen flex justify-center items-center">
+        <CircularProgress />
+      </div>
+    </>
+  );
+  return (
+    <>
+      <div className="container mx-auto">
+        <div className="grid grid-cols-10">
+          <div className="col-start-3 col-span-6">
+            {user && <EditUserForm user={user} />}
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default page;
