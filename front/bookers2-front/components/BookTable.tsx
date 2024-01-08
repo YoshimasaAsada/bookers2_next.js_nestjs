9d970befeeec4c617ddf78bf44386864eed49f91
YoshimasaAsada <yoshimasa111112@gmail.com>
2024-01-09 04:49:42 +0900

    ログインユーザーの投稿したもののみ消せるように変更

--- front/bookers2-front/components/BookTable.tsx
+++ front/bookers2-front/components/BookTable.tsx
@@ -1,3 +1,4 @@
+import { useQueryUser } from "@/hooks/useQueryUser";
 import { Link } from "@mui/material";
 import { Book, User } from "@prisma/client";
 import axios from "axios";
@@ -30,6 +31,44 @@ const BookTable = (props: any) => {
             {props.allBooks.map(
               (book: Book & { user: User }, index: number) => {
                 // なんかきもいから後で直す
+                if (props.loginUser.id === book.user.id)
+                  return (
+                    <tr className="border-b dark:border-gray-700" key={index}>
+                      <th
+                        scope="row"
+                        className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                      >
+                        <Link href={`/user/${book.user.id}`}>
+                          {book.user.name}
+                        </Link>
+                      </th>
+                      <td className="px-6 py-4">
+                        <Link href={`/book/${book.id}`}>{book.title}</Link>
+                      </td>
+                      <td className="px-6 py-4">{book.body}</td>
+                      <td className="px-6 py-4">
+                        <Link
+                          style={{ color: "white" }}
+                          href={`/book/${book.id}/edit`}
+                          type="button"
+                          className="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
+                        >
+                          Edit
+                        </Link>
+                      </td>
+                      <td className="px-6 py-4">
+                        <Link
+                          style={{ color: "white" }}
+                          // href={`/book/${book.id}/edit`}
+                          type="button"
+                          onClick={() => props.onClickDelete(book.id)}
+                          className="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
+                        >
+                          Delete
+                        </Link>
+                      </td>
+                    </tr>
+                  );
                 return (
                   <tr className="border-b dark:border-gray-700" key={index}>
                     <th
@@ -44,27 +83,6 @@ const BookTable = (props: any) => {
                       <Link href={`/book/${book.id}`}>{book.title}</Link>
                     </td>
                     <td className="px-6 py-4">{book.body}</td>
-                    <td className="px-6 py-4">
-                      <Link
-                        style={{ color: "white" }}
-                        href={`/book/${book.id}/edit`}
-                        type="button"
-                        className="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
-                      >
-                        Edit
-                      </Link>
-                    </td>
-                    <td className="px-6 py-4">
-                      <Link
-                        style={{ color: "white" }}
-                        // href={`/book/${book.id}/edit`}
-                        type="button"
-                        onClick={() => props.onClickDelete(book.id)}
-                        className="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
-                      >
-                        Delete
-                      </Link>
-                    </td>
                   </tr>
                 );
               }
