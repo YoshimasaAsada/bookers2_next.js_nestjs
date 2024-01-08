e312be81bfcfdeaf243f67d734aa46eb30c5eccd
YoshimasaAsada <yoshimasa111112@gmail.com>
2024-01-09 03:37:36 +0900

    headerをログイン時、ログアウト時で分けられるよう修正

--- front/bookers2-front/components/header.tsx
+++ front/bookers2-front/components/header.tsx
@@ -1,73 +1,80 @@
+import { useQueryUser } from "@/hooks/useQueryUser";
+import { useQueryClient } from "@tanstack/react-query";
 import axios from "axios";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 
 export const Header = () => {
+  const queryClient = useQueryClient();
+  // ログイン時とそうでない時でヘッダーを分けるためにログインユーザーとってる
+  const { queryLoginUser } = useQueryUser();
+  const { data } = queryLoginUser();
   const router = useRouter();
-  const onClickLogout = () => {
-    axios.post(`${process.env.NEXT_PUBLIC_API_URL}/auth/logout`);
-    router.push("/");
+
+  const onClickLogout = async () => {
+    // queryClient.removeQueries(["login-user"]);
+    // これだと再読み込みが走らない
+    await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/auth/logout`);
+    queryClient.setQueryData(["login-user"], null); // ログインユーザーのキャッシュをリセット
+    router.push("/log-in");
+    queryClient.invalidateQueries(["login-user"]); // キャッシュを無効化して再取得をトリガー
   };
 
   return (
     <header className="shadow-md py-4 px-4 sm:px-10 bg-dark font-[sans-serif] min-h-[70px]">
       <div className="flex flex-wrap items-center justify-between gap-5 relative">
         <img className="w-36" />
-
         <div className="flex lg:order-1 max-sm:ml-auto">
-          <Link
-            className="px-4 py-2 text-sm rounded-full font-bold text-white bg-indigo-600 transition-all ease-in-out duration-300 hover:bg-transparent hover:text-[indigo-600] ml-3"
-            href="log-in"
-          >
-            Login
-          </Link>
-          <Link
-            className="px-4 py-2 text-sm rounded-full font-bold text-white bg-indigo-600 transition-all ease-in-out duration-300 hover:bg-transparent hover:text-[indigo-600] ml-3"
-            href="sign-up"
-          >
-            Sign up
-          </Link>
-          <button
-            onClick={onClickLogout}
-            className="px-4 py-2 text-sm rounded-full font-bold text-white bg-indigo-600 transition-all ease-in-out duration-300 hover:bg-transparent hover:text-[indigo-600] ml-3"
-          >
-            Logout
-          </button>
+          {data ? (
+            <>
+              <Link
+                className="px-4 py-2 lg:hover:text-indigo-600 text-gray-500 block font-semibold text-[15px]"
+                href={`/user/${data.id}`}
+              >
+                Home
+              </Link>
+              <Link
+                className="px-4 py-2 lg:hover:text-indigo-600 text-gray-500 block font-semibold text-[15px]"
+                href="/user"
+              >
+                Users
+              </Link>
+              <Link
+                className="px-4 py-2 lg:hover:text-indigo-600 text-gray-500 block font-semibold text-[15px]"
+                href="/book"
+              >
+                Books
+              </Link>
+              <button
+                onClick={onClickLogout}
+                className="px-4 py-2 text-sm rounded-full font-bold text-white bg-indigo-600 transition-all ease-in-out duration-300 hover:bg-transparent hover:text-[indigo-600] ml-3"
+              >
+                Logout
+              </button>
+            </>
+          ) : (
+            <>
+              <Link
+                className="px-4 py-2 lg:hover:text-indigo-600 text-gray-500 block font-semibold text-[15px]"
+                href="/"
+              >
+                Top
+              </Link>
+              <Link
+                className="px-4 py-2 text-sm rounded-full font-bold text-white bg-indigo-600 transition-all ease-in-out duration-300 hover:bg-transparent hover:text-[indigo-600] ml-3"
+                href="log-in"
+              >
+                Login
+              </Link>
+              <Link
+                className="px-4 py-2 text-sm rounded-full font-bold text-white bg-indigo-600 transition-all ease-in-out duration-300 hover:bg-transparent hover:text-[indigo-600] ml-3"
+                href="sign-up"
+              >
+                Sign up
+              </Link>
+            </>
+          )}
         </div>
-        <ul
-          id="collapseMenu"
-          className="lg:!flex lg:space-x-5 max-lg:space-y-2 max-lg:hidden max-lg:py-4 max-lg:w-full"
-        >
-          <li className="max-lg:border-b max-lg:py-2 px-3 max-lg:rounded">
-            <Link
-              className="lg:hover:text-indigo-600 text-gray-500 block font-semibold text-[15px]"
-              href="/book"
-            >
-              Books
-            </Link>
-          </li>
-          <li className="max-lg:border-b max-lg:py-2 px-3 max-lg:rounded">
-            <Link
-              className="lg:hover:text-indigo-600 text-gray-500 block font-semibold text-[15px]"
-              href="/user"
-            >
-              Users
-            </Link>
-          </li>
-          <li className="max-lg:border-b max-lg:py-2 px-3 max-lg:rounded">
-            <Link
-              className="lg:hover:text-indigo-600 text-gray-500 block font-semibold text-[15px]"
-              href="/"
-            >
-              Top
-            </Link>
-          </li>
-          <li className="max-lg:border-b max-lg:py-2 px-3 max-lg:rounded">
-            <a className="lg:hover:text-indigo-600 text-gray-500 block font-semibold text-[15px]">
-              About
-            </a>
-          </li>
-        </ul>
       </div>
     </header>
   );
