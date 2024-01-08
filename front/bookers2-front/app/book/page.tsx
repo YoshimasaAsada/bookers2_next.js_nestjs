d4d2a72be0d6a870fbeb59f990216314b461e6e9
YoshimasaAsada <yoshimasa111112@gmail.com>
2024-01-09 06:32:41 +0900

    createのロジック変更に伴うpropsの削除

--- front/bookers2-front/app/book/page.tsx
+++ front/bookers2-front/app/book/page.tsx
@@ -4,26 +4,40 @@ import CreateBookForm from "@/components/CreateBookForm";
 import UserInfo from "@/components/UserInfo";
 import useMutateBook from "@/hooks/useMutateBook";
 import { useQueryBook } from "@/hooks/useQueryBook";
+import { useQueryUser } from "@/hooks/useQueryUser";
+import { CircularProgress } from "@mui/material";
 import React, { useEffect, useState } from "react";
 
 const page = () => {
   const { createBookMutation, deleteBookMutation } = useMutateBook();
-  const { data, status } = useQueryBook();
-  const currentUser = data?.currentUser ?? {};
-  // data?.currentUserの?消すと死ぬ
-  const books = data?.allBooks ?? [];
+  const { queryAllBook } = useQueryBook();
+  const { data: booksData, status } = queryAllBook();
+  const { queryLoginUser } = useQueryUser();
+  const { data: loginUserData, status: user } = queryLoginUser();
 
-  if (status === "loading") return <p>Loading...</p>;
+  // data?.currentUserの?消すと死ぬ
+  const books = booksData ?? [];
+  const loginUser = loginUserData ?? "";
+
+  if (status === "loading")
+    return (
+      <>
+        <div className="h-screen w-screen flex justify-center items-center">
+          <CircularProgress />
+        </div>
+      </>
+    );
   return (
     <>
       <div className="container mx-auto">
         <div className="grid grid-cols-10">
           <div className="col-start-1 col-span-3">
-            <UserInfo user={currentUser} />
-            <CreateBookForm onSubmit={createBookMutation.mutate} />
+            <UserInfo user={loginUser} loginUser={loginUser} />
+            <CreateBookForm />
           </div>
           <div className="col-start-5 col-span-10">
             <BookTable
+              loginUser={loginUser}
               allBooks={books}
               onClickDelete={deleteBookMutation.mutate}
             />
