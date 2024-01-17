0e93daf6fc45b825d38c8bef53268285b9e47b6d
YoshimasaAsada <yoshimasa111112@gmail.com>
2024-01-17 21:16:54 +0900

    エラーが出た時にログイン画面に飛ばす

--- front/bookers2-front/hooks/useQueryUser.ts
+++ front/bookers2-front/hooks/useQueryUser.ts
@@ -6,19 +6,16 @@ import { useRouter } from "next/navigation";
 export const useQueryAllUsers = () => {
   const router = useRouter();
   const getAllUsers = async () => {
-    try {
-      const res = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/user`);
-      return res.data;
-    } catch (error: any) {
-      router.push("/log-in");
-    }
+    const res = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/user`);
+    return res.data;
   };
 
   return useQuery({
     queryKey: ["users"],
     queryFn: getAllUsers,
     onError: (err: any) => {
-      console.log(err);
+      if (err.response.status === 401 || err.response.status === 403)
+        router.push("/log-in");
     },
   });
 };
@@ -27,30 +24,23 @@ export const useQueryAllUsers = () => {
 export const useQueryUserById = (userId: any) => {
   const router = useRouter();
   const getUserById = async () => {
-    try {
-      const { data: userData } = await axios.get(
-        `${process.env.NEXT_PUBLIC_API_URL}/user/${userId}`
-      );
-      // 本にuserプロパティを追加
-      const booksWithUser = userData.books.map((book: any) => ({
-        ...book,
-        user: userData,
-      }));
-      return { ...userData, books: booksWithUser };
-    } catch (error) {
-      if (axios.isAxiosError(error)) {
-        router.push("/log-in");
-      } else {
-        throw error;
-      }
-    }
+    const { data: userData } = await axios.get(
+      `${process.env.NEXT_PUBLIC_API_URL}/user/${userId}`
+    );
+    // 本にuserプロパティを追加
+    const booksWithUser = userData.books.map((book: any) => ({
+      ...book,
+      user: userData,
+    }));
+    return { ...userData, books: booksWithUser };
   };
 
   return useQuery({
     queryKey: ["user", userId],
     queryFn: getUserById,
-    onError: (err) => {
-      console.log(err);
+    onError: (err: any) => {
+      if (err.response.status === 401 || err.response.status === 403)
+        router.push("/log-in");
     },
   });
 };
@@ -59,22 +49,19 @@ export const useQueryUserById = (userId: any) => {
 export const useQueryLoginUser = () => {
   const router = useRouter();
   const getLoginUser = async () => {
-    try {
-      const res = await axios.get(
-        `${process.env.NEXT_PUBLIC_API_URL}/user/login-user`
-      );
-      console.log("test");
-      return res.data;
-    } catch (error: any) {
-      router.push("/log-in");
-    }
+    const res = await axios.get(
+      `${process.env.NEXT_PUBLIC_API_URL}/user/login-user`
+    );
+    return res.data;
   };
 
   return useQuery({
     queryKey: ["login-user"],
     queryFn: getLoginUser,
     onError: (err: any) => {
-      console.log(err);
+      if (err.response.status === 401 || err.response.status === 403)
+        console.log("errorchatch");
+      router.push("/log-in");
     },
   });
 };
